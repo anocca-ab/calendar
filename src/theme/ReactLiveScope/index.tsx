ed1c47c260a0abdb66d0afec4d1e5b0720ba3d6e
Ben Palæologos <biniam.palaiologos@anocca.com>
2024-05-06 14:45:49 +0200

    init set up

---
+++ src/theme/ReactLiveScope/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { CalendarEntry } from "@site/src/components/calendar/calendar_entry";
+
+// Add react-live imports you need here
+const ReactLiveScope = {
+  React,
+  ...React,
+  CalendarEntry,
+};
+
+export default ReactLiveScope;
