fb7b98ebfadf9bff281214d09569edc668332c11
Richie <ricsam@users.noreply.github.com>
2024-07-03 14:11:19 +0200

    fix for create event

--- nextjs/components/create_event.tsx
+++ nextjs/components/create_event.tsx
@@ -1,4 +1,4 @@
-import { isAllDayEvent } from "@/components/helpers";
+import { DEFAULT_COLOR, isAllDayEvent } from "@/components/helpers";
 import { CalendarEvent } from "@/components/types";
 import { FlexRow } from "@/components/wrappers";
 // import CheckCircleIcon from "@mui/icons-material/CheckCircle";
@@ -10,6 +10,7 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
+  Divider,
   FormControlLabel,
   FormGroup,
   MenuItem,
@@ -82,7 +83,7 @@ export function CreateEvent({
 
   const [title, setTitle] = React.useState(event.title ?? "");
 
-  const [eventColor, setEventColor] = React.useState(event.color ?? "#FF7043");
+  const [eventColor, setEventColor] = React.useState(event.color ?? DEFAULT_COLOR);
 
   const onChangeEventColor = (event: SelectChangeEvent) => {
     setEventColor(event.target.value);
@@ -346,7 +347,25 @@ export function CreateEvent({
           </FlexRow>
 
           <FlexRow pt={1} gap={2} alignItems="flex-end">
-            <Box width={24} />
+            <Box width={24}>
+              <Box
+                component="svg"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                sx={{ path: { fill: (theme) => theme.palette.text.primary } }}
+              >
+                <g>
+                  <path d="M12 22C6.49 22 2 17.51 2 12C2 6.49 6.49 2 12 2C17.51 2 22 6.04 22 11C22 14.31 19.31 17 16 17H14.23C13.95 17 13.73 17.22 13.73 17.5C13.73 17.62 13.78 17.73 13.86 17.83C14.27 18.3 14.5 18.89 14.5 19.5C14.5 20.88 13.38 22 12 22V22ZM12 4C7.59 4 4 7.59 4 12C4 16.41 7.59 20 12 20C12.28 20 12.5 19.78 12.5 19.5C12.5 19.34 12.42 19.22 12.36 19.15C11.95 18.69 11.73 18.1 11.73 17.5C11.73 16.12 12.85 15 14.23 15H16C18.21 15 20 13.21 20 11C20 7.14 16.41 4 12 4Z" />
+                  <path d="M6.5 13C7.32843 13 8 12.3284 8 11.5C8 10.6716 7.32843 10 6.5 10C5.67157 10 5 10.6716 5 11.5C5 12.3284 5.67157 13 6.5 13Z" />
+                  <path d="M9.5 9C10.3284 9 11 8.32843 11 7.5C11 6.67157 10.3284 6 9.5 6C8.67157 6 8 6.67157 8 7.5C8 8.32843 8.67157 9 9.5 9Z" />
+                  <path d="M14.5 9C15.3284 9 16 8.32843 16 7.5C16 6.67157 15.3284 6 14.5 6C13.6716 6 13 6.67157 13 7.5C13 8.32843 13.6716 9 14.5 9Z" />
+                  <path d="M17.5 13C18.3284 13 19 12.3284 19 11.5C19 10.6716 18.3284 10 17.5 10C16.6716 10 16 10.6716 16 11.5C16 12.3284 16.6716 13 17.5 13Z" />
+                </g>
+              </Box>
+            </Box>
 
             <Select
               variant="standard"
@@ -369,25 +388,32 @@ export function CreateEvent({
                 )
               )}
             </Select>
-            <Typography variant="body1">Event color</Typography>
           </FlexRow>
         </DialogContent>
         <DialogActions sx={{ justifyContent: "space-between" }}>
-          <Button
-            variant="contained"
-            color="error"
-            onClick={() => {
-              onDelete(event);
-              handleClose();
-            }}
-          >
-            Delete event
-          </Button>
-          <FlexRow>
+          <Box />
+          <FlexRow gap={1}>
+            <Button
+              variant="text"
+              color="error"
+              onClick={() => {
+                onDelete(event);
+                handleClose();
+              }}
+            >
+              Delete event
+            </Button>
+            <Divider flexItem {...{ orientation: "vertical" }} />
             <Button onClick={handleClose}>Cancel</Button>
             <Button
               type="submit"
               variant="contained"
+              disabled={
+                start.getTime() === event.start.getTime() &&
+                end?.getTime() === event.end?.getTime() &&
+                title === event.title &&
+                eventColor === (event.color ?? DEFAULT_COLOR)
+              }
               onClick={() => {
                 onSave(
                   {
